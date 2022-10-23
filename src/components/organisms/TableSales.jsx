b2b133529958ccdedcb6827a5b9b8fd7eab0bdc7
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2022-10-23 16:57:18 -0500

    Renderizado de tabla ventas con datos de local storage

--- src/components/organisms/TableSales.jsx
+++ src/components/organisms/TableSales.jsx
@@ -1,34 +1,47 @@
 import React from 'react';
 
 export const TableSales = () => {
+    const products = JSON.parse(localStorage.getItem('sales')) || [];
+
+    const unidades = products
+        .map((product) => parseInt(product.quantity))
+        .reduce((current, next) => current + next);
+    const total = products
+        .map((product) => product.total)
+        .reduce((current, next) => current + next);
+
     return (
         <table className="table table-striped">
             <caption>Lista de ventas diarias</caption>
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Primero</th>
-                    <th scope="col">Último</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Producto</th>
+                    <th scope="col">Precio unitario</th>
+                    <th scope="col">Unidades vendidas</th>
+                    <th scope="col">Precio total</th>
+                    <th scope="col">Fecha</th>
                 </tr>
             </thead>
             <tbody>
+                {products.map((product, key) => (
+                    <tr key={key}>
+                        <th scope="row">{key + 1}</th>
+                        <td>{product.product}</td>
+                        <td>{product.price}</td>
+                        <td>{product.quantity}</td>
+                        <td>{product.total}</td>
+                        <td>{product.date}</td>
+                    </tr>
+                ))}
+
                 <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colSpan={2}>Larry the Bird</td>
-                    <td>@twitter</td>
+                    <th scope="row">*</th>
+                    <td colSpan={2}>
+                        <b>Total</b>
+                    </td>
+                    <td>{unidades} </td>
+                    <td colSpan={2}>{total}</td>
                 </tr>
             </tbody>
         </table>
